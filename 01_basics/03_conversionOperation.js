97904ac8d8e29318c73c6f330580893ac8d54938
iamgauravkr <141003588+iamgauravkr@users.noreply.github.com>
2023-10-03 10:34:15 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,43 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNumber=33
 
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ************************Operations***********************
+
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(3%2);
+
+let str1="gaurav"
+let str2=" sonu"
+let str3=str1 + str2
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 =2+2;
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+
+//read prefix and post through mdn link given below
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
