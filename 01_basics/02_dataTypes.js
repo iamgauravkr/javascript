f86f5b999f7a94062b2a2b66518753f42f6787e6
iamgauravkr <141003588+iamgauravkr@users.noreply.github.com>
2023-10-03 07:40:41 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)//we are using node js, not browser
+
+console.log(3
+    +
+    3) // code readability should be high 
+    console.log("gaurav");
+
+    let name="Sonu"
+    let age=22
+    let isLoggedIn=false
+
+    //number => 2 to the power 53
+    //bigint
+    //string=> ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined
+    //symbol =>unique
+
+
+    console.log(typeof "gaurav"); //string
+    console.log(typeof 22); //number
+    console.log(typeof null); //object
+    console.log(typeof undefined); //undefined
+    
