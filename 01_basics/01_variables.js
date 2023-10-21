cd81ab72522a0aaff9b0af709287eda3d0e40417
iamgauravkr <141003588+iamgauravkr@users.noreply.github.com>
2023-10-21 13:12:11 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,7 @@ let Accountemail= "gaurav141052003@gmail.com"
 var AccountPassword=14052003
 AccountCity = "Mahua"
 
-//AccountId=2//not allowed as we can't chane the value of constant 
+//AccountId=2//not allowed as we can't change the value of constant 
 Accountemail="gk900@gmail.com"
 AccountPassword=147895
 AccountCity="hajipur"
