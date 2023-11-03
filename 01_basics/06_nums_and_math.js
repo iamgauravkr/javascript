ecb3e272310eae2fbf86beba29721251c292b466
iamgauravkr <141003588+iamgauravkr@users.noreply.github.com>
2023-11-03 07:14:32 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,27 @@
+const score= 400 
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds =1000000
+//console.log(hundreds.toLocaleString('en-In'));
+
+//  ++++++++++++++++++++++++   Maths  +++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(6.2));
+// console.log(Math.min(4,6,8,2,9));
+// console.log(Math.max(65,865,84,947));
+
+console.log(Math.random());
