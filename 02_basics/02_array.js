cfafe9e52c2e282b897c8e090a3546081ada762c
iamgauravkr <141003588+iamgauravkr@users.noreply.github.com>
2023-11-08 18:58:17 +0000

    another array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,9 @@
+const marvel_heros = ["iron-man","thor","spiderman","doctor-strange"]
+const dc_heros = ["batman","superman","flash"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[4][1]);  //superman
+
+const allheros = marvel_heros.concat(dc_heros)
+console.log(allheros);
