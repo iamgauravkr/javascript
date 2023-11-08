f38c845dbfc5f980a6bdc2ee8a1e751aef0d0e32
iamgauravkr <141003588+iamgauravkr@users.noreply.github.com>
2023-11-08 05:43:23 +0000

    updatedDates

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -16,7 +16,20 @@ let myDate = new Date()
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
-console.log(Math.floor(Date.now()/1000)); 
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000)); 
 
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth()+1);
+
+// console.log(`${newDate.getDay()} and the time is`);
+
+
+//  console.log(newDate.toLocaleString('deafult',{
+//     weekday:"long",
+//     month:"long",
+//     year:"numeric"
+// }));  
