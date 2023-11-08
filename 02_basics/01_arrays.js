e7a8152f3d22c8fb6fc1655dd06fc893a589a7f3
iamgauravkr <141003588+iamgauravkr@users.noreply.github.com>
2023-11-08 17:03:28 +0000

    Array updated

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,2 +1,17 @@
 //Arrays
+// JavaScript arrays are resizable and can contain a mix of different data types.
+//JavaScript arrays are zero-indexed.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
+const myArray=[0,1,2,3,5,5,3]
+const myHeros = ["shaktiman","nagraj","goga"]
+
+const myArray2 = new Array(9,4,3,5,6,8,2,0)
+console.log(myArray[6]);
+
+//Array method 
+// myArray.push(9)
+// myArray.pop()
+// myArray.push(10)
+myArray.
+console.log(myArray);
